a5d0a291e43fe03eb9666635795fd055a019820e
Donald Wilcox <dowilcox@umich.edu>
2026-03-24 20:33:19 -0400

    fix: hard reload on CSRF token mismatch (419) to refresh session

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -2,3 +2,15 @@ import axios from "axios";
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+// On CSRF token mismatch (419), hard reload to get a fresh token
+window.axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 419) {
+            window.location.reload();
+            return new Promise(() => {});
+        }
+        return Promise.reject(error);
+    },
+);
